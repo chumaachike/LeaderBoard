6f716021ffa0a8013e010c84f5755d6747e92695
unknown <edwardachike@gmail.com>
2022-06-18 07:36:10 -0700

    fixing linter errors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import "./index.css";
-import API from "./modules/API.js";
+import './index.css';
+import API from './modules/API';
 
 // gameID = 'sXfepIsNzVIibJ7qdAL3';
 const api = new API();
-const submitScore = document.getElementById("submitScore");
-const refreshButton = document.getElementById("refreshButton");
-const scores = document.getElementById("scores");
+const submitScore = document.getElementById('submitScore');
+const refreshButton = document.getElementById('refreshButton');
+const scores = document.getElementById('scores');
 
-window.addEventListener("DOMContentLoaded", () => {
-  submitScore.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const playerName = document.getElementById("playerName").value;
-    const playerScore = document.getElementById("playerScore").value;
-    api.addNewScore(playerName, playerScore);
-    populateScores();
-  });
-
+window.addEventListener('DOMContentLoaded', () => {
   const populateScores = async () => {
     const gamescores = await api.getGameScore();
     gamescores.result.forEach((gamescore) => {
-      const newScore = document.createElement("li");
+      const newScore = document.createElement('li');
       newScore.innerHTML = `${gamescore.user}: ${gamescore.score}`;
       scores.append(newScore);
     });
   };
-  refreshButton.addEventListener("click", populateScores);
+
+  submitScore.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const playerName = document.getElementById('playerName').value;
+    const playerScore = document.getElementById('playerScore').value;
+    api.addNewScore(playerName, playerScore);
+    populateScores();
+  });
+  
+  refreshButton.addEventListener('click', populateScores);
   populateScores();
 });
